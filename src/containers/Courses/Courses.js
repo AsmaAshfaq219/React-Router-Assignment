674504256ae2c0be069be2c578f5e095b379c548
AsmaAshfaq219 <asmaashfaq123@gmail.com>
2018-04-04 02:22:12 +0500

    React Router Assignment

---
+++ src/containers/Courses/Courses.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+import Course from '../Course/Course';
+import './Courses.css';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+
+                <section className="Courses">
+                    {
+                        this.state.courses.map(course => {
+                            return <article
+                                className="Course"
+                                key={course.id}
+                                onClick={() => {
+                                    this.props.history.push({
+                                        pathname: '/' + course.id + '/' + course.title,
+                                    })
+                                }}>
+                                {course.title}</article>
+                        })
+                    }
+                </section>
+                <Route path='/:id/:title' component={Course} />
+            </div>
+        );
+    }
+}
+
+export default Courses;
